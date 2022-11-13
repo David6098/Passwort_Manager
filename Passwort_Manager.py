16a4bb524168b563114a23a57227aef2ce245d59
David Seitz <seitzda1@infolernen.de>
2022-11-13 12:01:34 +0100

    Missing Buttons added (not looking good)

--- Passwort_Manager.py
+++ Passwort_Manager.py
@@ -15,9 +15,15 @@ class Passwort_Manager:
         self.root.geometry('600x400+50+50')
         passwHinzu = ttk.Button(self.root, text='Passwort hinzufügen', command=lambda: self.Passwort_Hinzufuegen())
         passwAus = ttk.Button(self.root, text='Passwort ausgeben', command=lambda: self.Passwort_Ausgeben())
+        allePasswaus = ttk.Button(self.root, text= 'Alle Passwörter ausgeben', command=lambda: self.alleElementeAusgeben())
+        PasswLoeschen = ttk.Button(self.root, text= 'Passwort löschen', command=lambda: self.ElementLoeschen())
+        allepasswloeschen = ttk.Button(self.root, text='Alle Passwörter löschen', command=lambda: self.alleElementeLoeschen())
         Schliessen = ttk.Button(self.root, text='Schließen', command=lambda: self.root.quit())
         passwHinzu.pack(ipadx=5, ipady=5, expand=True)
         passwAus.pack(ipadx=5, ipady=5, expand=True)
+        allePasswaus.pack(ipadx=5, ipady=5, expand=True)
+        PasswLoeschen.pack(ipadx=5, ipady=5, expand=True)
+        allepasswloeschen.pack(ipadx=5, ipady=5, expand=True)
         Schliessen.pack(ipadx=5, ipady=5, expand=True)
         self.root.mainloop()
 
@@ -59,28 +65,105 @@ class Passwort_Manager:
             passwHinzu.destroy()
 
     def Passwort_Ausgeben(self):
-        website = input('Von welcher Website wollen Sie das Passwort?  ')
-        listenel = self.ListenelementGeben(website)
-        if listenel is None:
-            raise Exception('Für die Website ' + website + ' existiert kein Eintrag')
-        w, user, passw = listenel
-        print('Website: ' + str(website) + '\n' + 'Benutzername: ' + str(user) + '\n' + 'Passwort: ' + str(passw))
+        passwaus = Tk()
+        web = tkinter.StringVar()
+        passwaus.title('Passwort ausgeben')
+        passwaus.geometry('300x150')
+        website_label = ttk.Label(passwaus, text="Website:")
+        website_label.pack(fill='x', expand=True)
+        website_entry = ttk.Entry(passwaus, textvariable=web)
+        website_entry.pack(fill='x', expand=True)
+        Fertig = ttk.Button(passwaus, text='Fertig', command=lambda: return_Passwort())
+        Fertig.pack(fill='x', expand=True)
+        def return_Passwort():
+            website = website_entry.get()
+            listenel = self.ListenelementGeben(website)
+            if listenel is None:
+                tkinter.messagebox.showerror(title='Passwort ausgeben', message='Für die Website ' + website + ' existiert kein Eintrag')
+            else:
+                w, user, passw = listenel
+                # print('Website: ' + str(website) + '\n' + 'Benutzername: ' + str(user) + '\n' + 'Passwort: ' + str(passw))
+                website_entry.destroy()
+                website_entry_2 = ttk.Label(passwaus, text=w)
+                website_entry_2.pack(fill='x', expand=True)
+                user_label = ttk.Label(passwaus, text="Benutzername/ Email:")
+                user_label.pack(fill='x', expand=True)
+
+                user_entry = ttk.Label(passwaus, text=user)
+                user_entry.pack(fill='x', expand=True)
+                # Passwort
+                passw_label = ttk.Label(passwaus, text="Passwort:")
+                passw_label.pack(fill='x', expand=True)
+
+                passw_entry = ttk.Label(passwaus, text=passw)
+                passw_entry.pack(fill='x', expand=True)
+                Fertig.destroy()
+                Leave = ttk.Button(passwaus, text='Fertig', command=lambda: passwaus.destroy())
+                Leave.pack(fill='x', expand=True)
 
     def alleElementeAusgeben(self):
+        elementeListe = []
+        passwaus = Tk()
+        passwaus.title('Alle Passwörter')
         i = 0
         for element in self.Passwortliste:
             web, user, passw = element
             i += 1
+            entry = ttk.Label(passwaus,text=str(i)+'. Eintrag:')
+            entry.pack(fill='x', expand=True)
+            # Website
+
+            website_label = ttk.Label(passwaus, text="      Website:")
+            website_label.pack(fill='x', expand=True)
+
+            website_entry_2 = ttk.Label(passwaus, text="            "+web)
+            website_entry_2.pack(fill='x', expand=True)
+            # Username
+            user_label = ttk.Label(passwaus, text="      Benutzername/ Email:")
+            user_label.pack(fill='x', expand=True)
+
+            user_entry = ttk.Label(passwaus, text="            "+user)
+            user_entry.pack(fill='x', expand=True)
+            # Passwort
+            passw_label = ttk.Label(passwaus, text="      Passwort:")
+            passw_label.pack(fill='x', expand=True)
+
+            passw_entry = ttk.Label(passwaus, text="            "+passw)
+            passw_entry.pack(fill='x', expand=True)
+
+            Abschluss = Canvas(passwaus, height=5, width=150, bg='black')
+            Abschluss.pack(fill='x', expand=True)
+            elementeListe.append((entry, website_label, website_entry_2, user_label, user_entry, passw_label, passw_entry))
+
             print(str(i) + '.Eintrag \n   Website: ' + str(web) + '\n' + '   Benutzername: ' + str(user) + '\n' + '   Passwort: ' + str(passw))
+        # Fertig
+        Leave = ttk.Button(passwaus, text='Fertig', command=lambda: passwaus.destroy())
+        Leave.pack(fill='x', expand=True)
 
+        geo = '300x' + str(150*len(elementeListe))
+        passwaus.geometry(geo)
     def ElementLoeschen(self):
-        web = input('Welches Passwort möchten sie löschen? ')
-        ele = self.ListenelementGeben(web)
-        if ele is None:
-            raise Exception('Für die Website ' + web + ' existiert kein Eintrag')
-        self.Passwortliste.remove(ele)
-        self.Aenderungen_Speichern()
-
+        passwaus = Tk()
+        webe = tkinter.StringVar()
+        passwaus.title('Passwort löschen')
+        passwaus.geometry('300x150')
+        website_label = ttk.Label(passwaus, text="Von welcher Website möchten Sie den Eintrag löschen?")
+        website_label.pack(fill='x', expand=True)
+        website_entry = ttk.Entry(passwaus, textvariable=webe)
+        website_entry.pack(fill='x', expand=True)
+        button = ttk.Button(passwaus, text='Passwort löschen', command=lambda: passwortandern())
+        button.pack(fill='x', expand=True)
+        def passwortandern():
+            web = website_entry.get()
+            passwaus.destroy()
+            ele = self.ListenelementGeben(web)
+            if ele is None:
+                tkinter.messagebox.showinfo('Passwort_Manager', 'Für die Website ' + web + ' existiert kein Eintrag')
+            else:
+                result = tkinter.messagebox.askquestion('Passwort_Manager', 'Sind Sie sicher? ')
+                if result == 'yes':
+                    self.Passwortliste.remove(ele)
+                    self.Aenderungen_Speichern()
     def alleElementeLoeschen(self):
         result = tkinter.messagebox.askquestion('Passwort_Manager', 'Sind Sie sicher? ')
         if result == 'yes':
@@ -88,8 +171,7 @@ class Passwort_Manager:
             self.Aenderungen_Speichern()
             theLabel = Label(self.root, text="Liste gelöscht.")
             theLabel.pack()
-        else:
-            self.root.destroy()  # Closing Tkinter window forcefully.
+
 
 
     def ListenelementGeben(self, website):
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Passwort_Manager
 My plan is to create a password manager, which encrypts passwords locally and automatically fills them into the Browser.
+
+What's missing:
+	- encryption
+	- a lot of bottons
+	- an appealing appearence
